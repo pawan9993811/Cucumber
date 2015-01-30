e75376d2c93ca7e237b8688ecf9c149bda22706b
pawan9993811 <pawan9993811@gmail.com>
2015-01-30 18:37:33 +0530

    Change in Run

--- cucumber-java-template/src/test/java/step_definitions/RunCukesTest.java
+++ cucumber-java-template/src/test/java/step_definitions/RunCukesTest.java
@@ -8,10 +8,10 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:features",
-		plugin = {"pretty", "html:target/cucumber-html-report"},
-		tags = {}
-		)
+		format =
+{"pretty", "html:target/cucumber","json:target/cucumber.json"})
+		
+		
 public class RunCukesTest{
 	
 }
